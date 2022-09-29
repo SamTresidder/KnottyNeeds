16ca9dfcb8a9003c826adb5d2bc4d150f8743356
SamTresidder <samtresidder@gmail.com>
2022-09-29 13:19:50 -0400

    Added gallery hover css

--- app.js
+++ app.js
@@ -1,45 +1,40 @@
 
-addEventListener('click', () => {
-    let img1 = document.body.getElementsByClassName('img-1');
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img1.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img2 = document.body.getElementsByClassName('img-2');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img2.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img3 = document.body.getElementsByClassName('img-3');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img3.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img4 = document.body.getElementsByClassName('img-4');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img4.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img5 = document.body.getElementsByClassName('img-5');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img5.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img6 = document.body.getElementsByClassName('img-6');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img6.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img7 = document.body.getElementsByClassName('img-7');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img7.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img8 = document.body.getElementsByClassName('img-8');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img8.jpg", "Window Title", "width=500, height=450");   
-})
-addEventListener('click', () => {
-    let img9 = document.body.getElementsByClassName('img-9');
-    console.log('You clicked the image')
-    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img9.jpg", "Window Title", "width=500, height=450");   
-})
+// function.showMyImage(){
+//     let img1 = document.getElementById('img-1');
+//     img1.src = "https://samtresidder.github.io/KnottyNeeds/gallery-img1.jpg";
+//     img1.stlye.display = "block"
+// }
+        // window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img1.jpg", "Window Title", "width=500, height=450");
+   
+// img2.addEventListener('click', () => {
+//     let img2 = document.body.getElementsByClassName('img-2');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img2.jpg", "Window Title", "width=500, height=450");   
+// })
+// img3.addEventListener('click', () => {
+//     let img3 = document.body.getElementsByClassName('img-3');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img3.jpg", "Window Title", "width=500, height=450");   
+// })
+// img4.addEventListener('click', () => {
+//     let img3 = document.body.getElementsByClassName('img-4');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img4.jpg", "Window Title", "width=500, height=450");   
+// })
+// img5.addEventListener('click', () => {
+//     let img3 = document.body.getElementsByClassName('img-5');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img5.jpg", "Window Title", "width=500, height=450");   
+// })
+// img6.addEventListener('click', () => {
+//     let img6 = document.body.getElementsByClassName('img-6');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img6.jpg", "Window Title", "width=500, height=450");   
+// })
+// img7.addEventListener('click', () => {
+//     let img7 = document.body.getElementsByClassName('img-7');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img7.jpg", "Window Title", "width=500, height=450");   
+// })
+// img8.addEventListener('click', () => {
+//     let img8 = document.body.getElementsByClassName('img-8');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img8.jpg", "Window Title", "width=500, height=450");   
+// })
+// img9.addEventListener('click', () => {
+//     let img9 = document.body.getElementsByClassName('img-9');
+//     window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img9.jpg", "Window Title", "width=500, height=450");   
+// })
