b8afab0778b1c34756c116cf3756cb44f16e72b4
SamTresidder <samtresidder@gmail.com>
2022-09-29 12:28:21 -0400

    added JavaScript for Gallery Images

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 
-addEventListener('load', (event) =>{
-    
-
-
+addEventListener('click', () => {
+    let img1 = document.body.getElementsByClassName('img-1');
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img1.jpg", "Window Title", "width=500, height=450");   
 })
-
-
+addEventListener('click', () => {
+    let img2 = document.body.getElementsByClassName('img-2');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img2.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img3 = document.body.getElementsByClassName('img-3');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img3.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img4 = document.body.getElementsByClassName('img-4');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img4.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img5 = document.body.getElementsByClassName('img-5');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img5.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img6 = document.body.getElementsByClassName('img-6');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img6.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img7 = document.body.getElementsByClassName('img-7');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img7.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img8 = document.body.getElementsByClassName('img-8');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img8.jpg", "Window Title", "width=500, height=450");   
+})
+addEventListener('click', () => {
+    let img9 = document.body.getElementsByClassName('img-9');
+    console.log('You clicked the image')
+    window.open("https://samtresidder.github.io/KnottyNeeds/gallery-img9.jpg", "Window Title", "width=500, height=450");   
+})
